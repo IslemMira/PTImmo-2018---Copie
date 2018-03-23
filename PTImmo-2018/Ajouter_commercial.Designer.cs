8de352bf56cf99a8e8fab5d7358fa4c5ab8a4650
mvellard-adm <mvellard-adm@INFO-DESIR>
2018-03-23 13:51:01 +0100

    Maxime 23/03/2018 Ajouter commercial

---
+++ PTImmo-2018/Ajouter_commercial.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PTImmo_2018
+{
+    partial class Ajouter_commercial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1_Nom = new System.Windows.Forms.TextBox();
+            this.textBox1_Prenom = new System.Windows.Forms.TextBox();
+            this.textBox1_adresse = new System.Windows.Forms.TextBox();
+            this.textBox1_FixePro = new System.Windows.Forms.TextBox();
+            this.textBox1_MobilePro = new System.Windows.Forms.TextBox();
+            this.textBox1_Tel_Prive = new System.Windows.Forms.TextBox();
+            this.textBox1_Email = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fiche du commercial à ajouter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prénom :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adresse : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Mobile pro :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Fixe pro :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 228);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Téléphone privé :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 266);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Email :";
+            // 
+            // textBox1_Nom
+            // 
+            this.textBox1_Nom.Location = new System.Drawing.Point(108, 54);
+            this.textBox1_Nom.Name = "textBox1_Nom";
+            this.textBox1_Nom.Size = new System.Drawing.Size(160, 20);
+            this.textBox1_Nom.TabIndex = 9;
+            // 
+            // textBox1_Prenom
+            // 
+            this.textBox1_Prenom.Location = new System.Drawing.Point(108, 83);
+            this.textBox1_Prenom.Name = "textBox1_Prenom";
+            this.textBox1_Prenom.Size = new System.Drawing.Size(160, 20);
+            this.textBox1_Prenom.TabIndex = 10;
+            // 
+            // textBox1_adresse
+            // 
+            this.textBox1_adresse.Location = new System.Drawing.Point(108, 118);
+            this.textBox1_adresse.Name = "textBox1_adresse";
+            this.textBox1_adresse.Size = new System.Drawing.Size(199, 20);
+            this.textBox1_adresse.TabIndex = 11;
+            // 
+            // textBox1_FixePro
+            // 
+            this.textBox1_FixePro.Location = new System.Drawing.Point(108, 160);
+            this.textBox1_FixePro.Name = "textBox1_FixePro";
+            this.textBox1_FixePro.Size = new System.Drawing.Size(146, 20);
+            this.textBox1_FixePro.TabIndex = 12;
+            // 
+            // textBox1_MobilePro
+            // 
+            this.textBox1_MobilePro.Location = new System.Drawing.Point(108, 190);
+            this.textBox1_MobilePro.Name = "textBox1_MobilePro";
+            this.textBox1_MobilePro.Size = new System.Drawing.Size(146, 20);
+            this.textBox1_MobilePro.TabIndex = 13;
+            // 
+            // textBox1_Tel_Prive
+            // 
+            this.textBox1_Tel_Prive.Location = new System.Drawing.Point(108, 225);
+            this.textBox1_Tel_Prive.Name = "textBox1_Tel_Prive";
+            this.textBox1_Tel_Prive.Size = new System.Drawing.Size(146, 20);
+            this.textBox1_Tel_Prive.TabIndex = 14;
+            // 
+            // textBox1_Email
+            // 
+            this.textBox1_Email.Location = new System.Drawing.Point(108, 258);
+            this.textBox1_Email.Name = "textBox1_Email";
+            this.textBox1_Email.Size = new System.Drawing.Size(146, 20);
+            this.textBox1_Email.TabIndex = 15;
+            // 
+            // Ajouter_commercial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.textBox1_Email);
+            this.Controls.Add(this.textBox1_Tel_Prive);
+            this.Controls.Add(this.textBox1_MobilePro);
+            this.Controls.Add(this.textBox1_FixePro);
+            this.Controls.Add(this.textBox1_adresse);
+            this.Controls.Add(this.textBox1_Prenom);
+            this.Controls.Add(this.textBox1_Nom);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ajouter_commercial";
+            this.Text = "Ajouter commercial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1_Nom;
+        private System.Windows.Forms.TextBox textBox1_Prenom;
+        private System.Windows.Forms.TextBox textBox1_adresse;
+        private System.Windows.Forms.TextBox textBox1_FixePro;
+        private System.Windows.Forms.TextBox textBox1_MobilePro;
+        private System.Windows.Forms.TextBox textBox1_Tel_Prive;
+        private System.Windows.Forms.TextBox textBox1_Email;
+    }
+}
---
+++ PTImmo-2018/Ajouter_commercial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTImmo_2018
+{
+    public partial class Ajouter_commercial : Form
+    {
+        public Ajouter_commercial()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
