3f9fe9d1a2ff6c288843f07307d4f369d176fc1d
Arina Moldovean <arina.moldovean@gmail.com>
2018-03-23 14:54:54 +0100

    FicheSouhaitsViz

---
+++ PTImmo-2018/FicheSouhaitsViz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTImmo_2018
+{
+	public partial class FicheSouhaitsViz : Form
+	{
+		public FicheSouhaitsViz()
+		{
+			InitializeComponent();
+		}
+
+		private void FicheSouhaitsViz_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
