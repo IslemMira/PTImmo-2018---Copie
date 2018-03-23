2269c557e6b5c59cb3d1034e12728bfc331af53b
Arina Moldovean <arina.moldovean@gmail.com>
2018-03-23 14:53:24 +0100

    FicheSouhaits

---
+++ PTImmo-2018/FicheSouhaitCreation.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace PTImmo_2018
+{
+	partial class FicheSouhaitCreation
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox_StatutSouhait = new System.Windows.Forms.ComboBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label26 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(437, 614);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 154;
+			this.button3.Text = "Annuler";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(317, 614);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 153;
+			this.button2.Text = "Valider";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// comboBox_StatutSouhait
+			// 
+			this.comboBox_StatutSouhait.FormattingEnabled = true;
+			this.comboBox_StatutSouhait.Items.AddRange(new object[] {
+            "En cours",
+            "Obsolete"});
+			this.comboBox_StatutSouhait.Location = new System.Drawing.Point(74, 545);
+			this.comboBox_StatutSouhait.Name = "comboBox_StatutSouhait";
+			this.comboBox_StatutSouhait.Size = new System.Drawing.Size(121, 21);
+			this.comboBox_StatutSouhait.TabIndex = 152;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(13, 546);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(37, 16);
+			this.label7.TabIndex = 151;
+			this.label7.Text = "Etat :";
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox2.Location = new System.Drawing.Point(16, 496);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(53, 19);
+			this.checkBox2.TabIndex = 150;
+			this.checkBox2.Text = "Cave";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox1.Location = new System.Drawing.Point(16, 461);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(67, 19);
+			this.checkBox1.TabIndex = 149;
+			this.checkBox1.Text = "Garage";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(15, 427);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(72, 16);
+			this.label6.TabIndex = 148;
+			this.label6.Text = "Prix (max) :";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(15, 383);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(201, 16);
+			this.label5.TabIndex = 147;
+			this.label5.Text = "Nombre de salles de bain (min) :";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(15, 344);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(158, 16);
+			this.label4.TabIndex = 146;
+			this.label4.Text = "Nombre de pieces (min) :";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(15, 305);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(144, 16);
+			this.label3.TabIndex = 145;
+			this.label3.Text = "Surface parcelle (min) :";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(138, 143);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(147, 95);
+			this.listBox1.TabIndex = 144;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(317, 100);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 143;
+			this.button1.Text = "Ajouter";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(138, 101);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(147, 20);
+			this.textBox1.TabIndex = 142;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(15, 101);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(116, 16);
+			this.label2.TabIndex = 141;
+			this.label2.Text = "Villes souhaitees :";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label17.Location = new System.Drawing.Point(15, 267);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(151, 16);
+			this.label17.TabIndex = 140;
+			this.label17.Text = "Surface habitable (min) :";
+			// 
+			// label26
+			// 
+			this.label26.AutoSize = true;
+			this.label26.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label26.Location = new System.Drawing.Point(12, 64);
+			this.label26.Name = "label26";
+			this.label26.Size = new System.Drawing.Size(154, 18);
+			this.label26.TabIndex = 139;
+			this.label26.Text = "Souhaits acheteur :";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(161, 20);
+			this.label1.TabIndex = 138;
+			this.label1.Text = "Fiche de souhaits :";
+			// 
+			// FicheSouhaitCreation
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(576, 726);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.comboBox_StatutSouhait);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.label26);
+			this.Controls.Add(this.label1);
+			this.Name = "FicheSouhaitCreation";
+			this.Text = "FicheSouhaitCreation";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ComboBox comboBox_StatutSouhait;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label26;
+		private System.Windows.Forms.Label label1;
+	}
+}
