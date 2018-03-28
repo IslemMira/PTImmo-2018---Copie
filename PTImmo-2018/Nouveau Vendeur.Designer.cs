e932ce70719ea58b5847f1c712a3c27b916d5886
mvellard-adm <mvellard-adm@INFO-DESIR>
2018-03-28 12:52:59 +0200

    Maxime 28/03/2018 Nouveau vendeur

---
+++ PTImmo-2018/Nouveau Vendeur.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace PTImmo_2018
+{
+    partial class Nouveau_Vendeur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox10_CP = new System.Windows.Forms.TextBox();
+            this.textBox12_Adresse = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Ville = new System.Windows.Forms.Label();
+            this.Adresse = new System.Windows.Forms.Label();
+            this.textBox4_E_Mail = new System.Windows.Forms.TextBox();
+            this.textBox3_Telephone = new System.Windows.Forms.TextBox();
+            this.textBox2_Prenom = new System.Windows.Forms.TextBox();
+            this.textBox1_Nom = new System.Windows.Forms.TextBox();
+            this.E_email = new System.Windows.Forms.Label();
+            this.Telephone = new System.Windows.Forms.Label();
+            this.Prenom = new System.Windows.Forms.Label();
+            this.Nom = new System.Windows.Forms.Label();
+            this.button_valider = new System.Windows.Forms.Button();
+            this.button_annuler = new System.Windows.Forms.Button();
+            this.comboBox1_Ville = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1_codeVille = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox10_CP
+            // 
+            this.textBox10_CP.Enabled = false;
+            this.textBox10_CP.Location = new System.Drawing.Point(444, 74);
+            this.textBox10_CP.Name = "textBox10_CP";
+            this.textBox10_CP.Size = new System.Drawing.Size(228, 20);
+            this.textBox10_CP.TabIndex = 59;
+            // 
+            // textBox12_Adresse
+            // 
+            this.textBox12_Adresse.Enabled = false;
+            this.textBox12_Adresse.Location = new System.Drawing.Point(444, 44);
+            this.textBox12_Adresse.Name = "textBox12_Adresse";
+            this.textBox12_Adresse.Size = new System.Drawing.Size(228, 20);
+            this.textBox12_Adresse.TabIndex = 57;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Cp:";
+            // 
+            // Ville
+            // 
+            this.Ville.AutoSize = true;
+            this.Ville.Location = new System.Drawing.Point(401, 105);
+            this.Ville.Name = "Ville";
+            this.Ville.Size = new System.Drawing.Size(29, 13);
+            this.Ville.TabIndex = 55;
+            this.Ville.Text = "Ville:";
+            // 
+            // Adresse
+            // 
+            this.Adresse.AutoSize = true;
+            this.Adresse.Location = new System.Drawing.Point(388, 48);
+            this.Adresse.Name = "Adresse";
+            this.Adresse.Size = new System.Drawing.Size(48, 13);
+            this.Adresse.TabIndex = 54;
+            this.Adresse.Text = "Adresse:";
+            // 
+            // textBox4_E_Mail
+            // 
+            this.textBox4_E_Mail.Enabled = false;
+            this.textBox4_E_Mail.Location = new System.Drawing.Point(81, 141);
+            this.textBox4_E_Mail.Name = "textBox4_E_Mail";
+            this.textBox4_E_Mail.Size = new System.Drawing.Size(228, 20);
+            this.textBox4_E_Mail.TabIndex = 53;
+            // 
+            // textBox3_Telephone
+            // 
+            this.textBox3_Telephone.Enabled = false;
+            this.textBox3_Telephone.Location = new System.Drawing.Point(81, 105);
+            this.textBox3_Telephone.Name = "textBox3_Telephone";
+            this.textBox3_Telephone.Size = new System.Drawing.Size(228, 20);
+            this.textBox3_Telephone.TabIndex = 52;
+            // 
+            // textBox2_Prenom
+            // 
+            this.textBox2_Prenom.Enabled = false;
+            this.textBox2_Prenom.Location = new System.Drawing.Point(81, 74);
+            this.textBox2_Prenom.Name = "textBox2_Prenom";
+            this.textBox2_Prenom.Size = new System.Drawing.Size(228, 20);
+            this.textBox2_Prenom.TabIndex = 51;
+            // 
+            // textBox1_Nom
+            // 
+            this.textBox1_Nom.Enabled = false;
+            this.textBox1_Nom.Location = new System.Drawing.Point(81, 44);
+            this.textBox1_Nom.Name = "textBox1_Nom";
+            this.textBox1_Nom.Size = new System.Drawing.Size(228, 20);
+            this.textBox1_Nom.TabIndex = 50;
+            // 
+            // E_email
+            // 
+            this.E_email.AutoSize = true;
+            this.E_email.Location = new System.Drawing.Point(7, 141);
+            this.E_email.Name = "E_email";
+            this.E_email.Size = new System.Drawing.Size(44, 13);
+            this.E_email.TabIndex = 49;
+            this.E_email.Text = "E-email:";
+            // 
+            // Telephone
+            // 
+            this.Telephone.AutoSize = true;
+            this.Telephone.Location = new System.Drawing.Point(7, 105);
+            this.Telephone.Name = "Telephone";
+            this.Telephone.Size = new System.Drawing.Size(61, 13);
+            this.Telephone.TabIndex = 48;
+            this.Telephone.Text = "Telephone:";
+            // 
+            // Prenom
+            // 
+            this.Prenom.AutoSize = true;
+            this.Prenom.Location = new System.Drawing.Point(7, 74);
+            this.Prenom.Name = "Prenom";
+            this.Prenom.Size = new System.Drawing.Size(46, 13);
+            this.Prenom.TabIndex = 47;
+            this.Prenom.Text = "Prenom:";
+            // 
+            // Nom
+            // 
+            this.Nom.AutoSize = true;
+            this.Nom.Location = new System.Drawing.Point(7, 44);
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(32, 13);
+            this.Nom.TabIndex = 46;
+            this.Nom.Text = "Nom:";
+            // 
+            // button_valider
+            // 
+            this.button_valider.Location = new System.Drawing.Point(557, 397);
+            this.button_valider.Name = "button_valider";
+            this.button_valider.Size = new System.Drawing.Size(75, 23);
+            this.button_valider.TabIndex = 61;
+            this.button_valider.Text = "VALIDER";
+            this.button_valider.UseVisualStyleBackColor = true;
+            // 
+            // button_annuler
+            // 
+            this.button_annuler.Location = new System.Drawing.Point(476, 397);
+            this.button_annuler.Name = "button_annuler";
+            this.button_annuler.Size = new System.Drawing.Size(75, 23);
+            this.button_annuler.TabIndex = 60;
+            this.button_annuler.Text = "ANNULER";
+            this.button_annuler.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1_Ville
+            // 
+            this.comboBox1_Ville.FormattingEnabled = true;
+            this.comboBox1_Ville.Location = new System.Drawing.Point(444, 105);
+            this.comboBox1_Ville.Name = "comboBox1_Ville";
+            this.comboBox1_Ville.Size = new System.Drawing.Size(228, 21);
+            this.comboBox1_Ville.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "Code ville:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1_codeVille
+            // 
+            this.textBox1_codeVille.Enabled = false;
+            this.textBox1_codeVille.Location = new System.Drawing.Point(444, 138);
+            this.textBox1_codeVille.Name = "textBox1_codeVille";
+            this.textBox1_codeVille.Size = new System.Drawing.Size(146, 20);
+            this.textBox1_codeVille.TabIndex = 64;
+            // 
+            // Nouveau_Vendeur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.textBox1_codeVille);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1_Ville);
+            this.Controls.Add(this.button_valider);
+            this.Controls.Add(this.button_annuler);
+            this.Controls.Add(this.textBox10_CP);
+            this.Controls.Add(this.textBox12_Adresse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ville);
+            this.Controls.Add(this.Adresse);
+            this.Controls.Add(this.textBox4_E_Mail);
+            this.Controls.Add(this.textBox3_Telephone);
+            this.Controls.Add(this.textBox2_Prenom);
+            this.Controls.Add(this.textBox1_Nom);
+            this.Controls.Add(this.E_email);
+            this.Controls.Add(this.Telephone);
+            this.Controls.Add(this.Prenom);
+            this.Controls.Add(this.Nom);
+            this.Name = "Nouveau_Vendeur";
+            this.Text = "Nouveau_Vendeur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox10_CP;
+        private System.Windows.Forms.TextBox textBox12_Adresse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Ville;
+        private System.Windows.Forms.Label Adresse;
+        private System.Windows.Forms.TextBox textBox4_E_Mail;
+        private System.Windows.Forms.TextBox textBox3_Telephone;
+        private System.Windows.Forms.TextBox textBox2_Prenom;
+        private System.Windows.Forms.TextBox textBox1_Nom;
+        private System.Windows.Forms.Label E_email;
+        private System.Windows.Forms.Label Telephone;
+        private System.Windows.Forms.Label Prenom;
+        private System.Windows.Forms.Label Nom;
+        private System.Windows.Forms.Button button_valider;
+        private System.Windows.Forms.Button button_annuler;
+        private System.Windows.Forms.ComboBox comboBox1_Ville;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1_codeVille;
+    }
+}
