ca2b7f1d2a7e3ebd292c88a420346b2649947db5
islama <islama@INFO-CHEVALERIE>
2018-04-03 15:26:04 +0200

    version03/04/18

--- PTImmo-2018/RechercheAcheteur.Designer.cs
+++ PTImmo-2018/RechercheAcheteur.Designer.cs
@@ -28,206 +28,207 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.radioButton1 = new System.Windows.Forms.RadioButton();
-			this.radioButton2 = new System.Windows.Forms.RadioButton();
-			this.label2 = new System.Windows.Forms.Label();
-			this.comboBox_commercial = new System.Windows.Forms.ComboBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.listBox1 = new System.Windows.Forms.ListBox();
-			this.button3 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(12, 22);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(169, 16);
-			this.label1.TabIndex = 122;
-			this.label1.Text = "Acheteur deja existant :";
-			// 
-			// radioButton1
-			// 
-			this.radioButton1.AutoSize = true;
-			this.radioButton1.Location = new System.Drawing.Point(39, 56);
-			this.radioButton1.Name = "radioButton1";
-			this.radioButton1.Size = new System.Drawing.Size(67, 17);
-			this.radioButton1.TabIndex = 123;
-			this.radioButton1.TabStop = true;
-			this.radioButton1.Text = "En cours";
-			this.radioButton1.UseVisualStyleBackColor = true;
-			// 
-			// radioButton2
-			// 
-			this.radioButton2.AutoSize = true;
-			this.radioButton2.Location = new System.Drawing.Point(39, 93);
-			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(86, 17);
-			this.radioButton2.TabIndex = 124;
-			this.radioButton2.TabStop = true;
-			this.radioButton2.Text = "Afficher Tout";
-			this.radioButton2.UseVisualStyleBackColor = true;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(201, 65);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(137, 16);
-			this.label2.TabIndex = 125;
-			this.label2.Text = "Filtrer par commercial";
-			// 
-			// comboBox_commercial
-			// 
-			this.comboBox_commercial.FormattingEnabled = true;
-			this.comboBox_commercial.Location = new System.Drawing.Point(204, 93);
-			this.comboBox_commercial.Name = "comboBox_commercial";
-			this.comboBox_commercial.Size = new System.Drawing.Size(134, 21);
-			this.comboBox_commercial.TabIndex = 126;
-			// 
-			// button1
-			// 
-			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(529, 65);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(150, 49);
-			this.button1.TabIndex = 127;
-			this.button1.Text = "Nouvel acheteur";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(36, 163);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(64, 16);
-			this.label3.TabIndex = 128;
-			this.label3.Text = "Identifiant";
-			this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(36, 202);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(37, 16);
-			this.label4.TabIndex = 129;
-			this.label4.Text = "Nom";
-			this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(36, 246);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(55, 16);
-			this.label5.TabIndex = 130;
-			this.label5.Text = "Prenom";
-			this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(127, 163);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(188, 20);
-			this.textBox1.TabIndex = 131;
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(127, 201);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(188, 20);
-			this.textBox2.TabIndex = 132;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(127, 245);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(188, 20);
-			this.textBox3.TabIndex = 133;
-			// 
-			// button2
-			// 
-			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(529, 216);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(150, 49);
-			this.button2.TabIndex = 134;
-			this.button2.Text = "Recherche";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// listBox1
-			// 
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(39, 328);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(640, 251);
-			this.listBox1.TabIndex = 135;
-			// 
-			// button3
-			// 
-			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button3.Location = new System.Drawing.Point(529, 617);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(150, 49);
-			this.button3.TabIndex = 136;
-			this.button3.Text = "Visualiser";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// RechercheAcheteur
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(738, 741);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.listBox1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.comboBox_commercial);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.radioButton2);
-			this.Controls.Add(this.radioButton1);
-			this.Controls.Add(this.label1);
-			this.Name = "RechercheAcheteur";
-			this.Text = "RechercheAcheteur";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.acheteur_d_exis = new System.Windows.Forms.Label();
+            this.en_cours = new System.Windows.Forms.RadioButton();
+            this.afficher_tout = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_commercial = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.identifiant = new System.Windows.Forms.TextBox();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // acheteur_d_exis
+            // 
+            this.acheteur_d_exis.AutoSize = true;
+            this.acheteur_d_exis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acheteur_d_exis.Location = new System.Drawing.Point(12, 22);
+            this.acheteur_d_exis.Name = "acheteur_d_exis";
+            this.acheteur_d_exis.Size = new System.Drawing.Size(169, 16);
+            this.acheteur_d_exis.TabIndex = 122;
+            this.acheteur_d_exis.Text = "Acheteur deja existant :";
+            // 
+            // en_cours
+            // 
+            this.en_cours.AutoSize = true;
+            this.en_cours.Location = new System.Drawing.Point(39, 56);
+            this.en_cours.Name = "en_cours";
+            this.en_cours.Size = new System.Drawing.Size(67, 17);
+            this.en_cours.TabIndex = 123;
+            this.en_cours.TabStop = true;
+            this.en_cours.Text = "En cours";
+            this.en_cours.UseVisualStyleBackColor = true;
+            // 
+            // afficher_tout
+            // 
+            this.afficher_tout.AutoSize = true;
+            this.afficher_tout.Location = new System.Drawing.Point(39, 93);
+            this.afficher_tout.Name = "afficher_tout";
+            this.afficher_tout.Size = new System.Drawing.Size(86, 17);
+            this.afficher_tout.TabIndex = 124;
+            this.afficher_tout.TabStop = true;
+            this.afficher_tout.Text = "Afficher Tout";
+            this.afficher_tout.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(201, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 16);
+            this.label2.TabIndex = 125;
+            this.label2.Text = "Filtrer par commercial";
+            // 
+            // comboBox_commercial
+            // 
+            this.comboBox_commercial.FormattingEnabled = true;
+            this.comboBox_commercial.Location = new System.Drawing.Point(204, 93);
+            this.comboBox_commercial.Name = "comboBox_commercial";
+            this.comboBox_commercial.Size = new System.Drawing.Size(134, 21);
+            this.comboBox_commercial.TabIndex = 126;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(529, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 49);
+            this.button1.TabIndex = 127;
+            this.button1.Text = "Nouvel acheteur";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "Identifiant";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 16);
+            this.label4.TabIndex = 129;
+            this.label4.Text = "Nom";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 130;
+            this.label5.Text = "Prenom";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // identifiant
+            // 
+            this.identifiant.Location = new System.Drawing.Point(127, 163);
+            this.identifiant.Name = "identifiant";
+            this.identifiant.Size = new System.Drawing.Size(188, 20);
+            this.identifiant.TabIndex = 131;
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(127, 201);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(188, 20);
+            this.nom.TabIndex = 132;
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(127, 245);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(188, 20);
+            this.prenom.TabIndex = 133;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(529, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 49);
+            this.button2.TabIndex = 134;
+            this.button2.Text = "Recherche";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(39, 328);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(640, 251);
+            this.listBox1.TabIndex = 135;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(529, 617);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 49);
+            this.button3.TabIndex = 136;
+            this.button3.Text = "Visualiser";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // RechercheAcheteur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 741);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.nom);
+            this.Controls.Add(this.identifiant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox_commercial);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.afficher_tout);
+            this.Controls.Add(this.en_cours);
+            this.Controls.Add(this.acheteur_d_exis);
+            this.Name = "RechercheAcheteur";
+            this.Text = "RechercheAcheteur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.RadioButton radioButton1;
-		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Label acheteur_d_exis;
+		private System.Windows.Forms.RadioButton en_cours;
+		private System.Windows.Forms.RadioButton afficher_tout;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.ComboBox comboBox_commercial;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox identifiant;
+		private System.Windows.Forms.TextBox nom;
+		private System.Windows.Forms.TextBox prenom;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.ListBox listBox1;
 		private System.Windows.Forms.Button button3;
--- PTImmo-2018/RechercheAcheteur.cs
+++ PTImmo-2018/RechercheAcheteur.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,67 @@ namespace PTImmo_2018
 {
 	public partial class RechercheAcheteur : Form
 	{
-		public RechercheAcheteur()
-		{
-			InitializeComponent();
-		}
-	}
-}
+        public RechercheAcheteur()
+        {
+            InitializeComponent();
+
+
+            string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-joyeux;Initial Catalog=IMMOBILLY_JACKYTEAM;Persist Security Info=True; Integrated Security=sspi;";
+            OleDbConnection dbConnection = new OleDbConnection(ChaineBd);
+            dbConnection.Open();
+            string sql = "SELECT num_acheteur, nom_Acheteur, prénom_Acheteur, adresse, téléphone, E_MAIL, VILLE.Nom_Ville from ACHETEUR left join SOUHAIT on ACHETEUR.NUM_ACHETEUR = SOUHAIT.NUM_ACHETEUR left join VILLE on ACHETEUR.Code_Ville = VILLE.Code_Ville where Nom_Acheteur like '%" + "' and Prénom_Acheteur like '%'" + "%' and statut like '%" + "';";
+
+            OleDbCommand cmd = new OleDbCommand(sql, dbConnection);
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                identifiant.Text = reader.GetString(1);
+                nom.Text = reader.GetString(2);
+                prenom.Text = reader.GetString(3);
+                listBox1.Text = reader.GetString(4);
+                //comboBox_commercial.
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nomBase = "IMMOBILLY_JACKYTEAM";
+            string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-joyeux;Initial Catalog=IMMOBILLY_JACKYTEAM;Persist Security Info=True; Integrated Security=sspi;";
+            OleDbConnection dbConnection = new OleDbConnection(ChaineBd);
+            dbConnection.Open();
+
+
+            string statuts = " ";
+            string sql = "SELECT num_acheteur, nom_Acheteur, prénom_Acheteur, adresse, téléphone, E_MAIL, VILLE.Nom_Ville from ACHETEUR left join SOUHAIT on ACHETEUR.NUM_ACHETEUR = SOUHAIT.NUM_ACHETEUR left join VILLE on ACHETEUR.Code_Ville = VILLE.Code_Ville where Nom_Acheteur like '%" + "' and Prénom_Acheteur like '%'" + "%' and statut like '%" + "';";
+            string acheteur_d_exis = "";
+
+            if (en_cours.Checked == true)
+            {
+                acheteur_d_exis = "E";
+                afficher_tout.Checked = false;
+            }
+
+            if (afficher_tout.Checked == true)
+            {
+                acheteur_d_exis = "A";
+                en_cours.Checked = false;
+            }
+       
+            private void Charger_Liste_Commerciaux()
+        {
+            List<Ajouter_commercial> commerciaux = (from c in RechercheAcheteur.IMMOBILLY_JACKYTEAM.COMMERCIAL
+                                            select c).ToList();
+
+            foreach (COMMERCIAL c in commerciaux)
+
+                comboBox_commercial.Items.Add(c.NOM_COMMERCIAL);
+           
+        }
+
+
+        OleDbCommand cmd = new OleDbCommand(sql, dbConnection);
+
+        }
+    }
+
