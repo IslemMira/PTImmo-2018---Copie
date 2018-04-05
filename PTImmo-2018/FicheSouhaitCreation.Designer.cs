0f1e2bae3b466d67631f96ebb631544dc6c3c3a6
fanguyen <fanguyen@INFO-MOLLY>
2018-04-05 15:48:56 +0200

    ajoutsouhait 05/04

--- PTImmo-2018/FicheSouhaitCreation.Designer.cs
+++ PTImmo-2018/FicheSouhaitCreation.Designer.cs
@@ -42,14 +42,17 @@
             this.label23 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button_Valider = new System.Windows.Forms.Button();
+            this.textBox_CP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nbPieces)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox_Ville
             // 
-            this.textBox_Ville.Location = new System.Drawing.Point(288, 329);
+            this.textBox_Ville.Location = new System.Drawing.Point(384, 405);
+            this.textBox_Ville.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_Ville.Name = "textBox_Ville";
-            this.textBox_Ville.Size = new System.Drawing.Size(147, 20);
+            this.textBox_Ville.Size = new System.Drawing.Size(195, 22);
             this.textBox_Ville.TabIndex = 142;
             // 
             // label2
@@ -57,40 +60,43 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(130, 330);
+            this.label2.Location = new System.Drawing.Point(173, 406);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.Size = new System.Drawing.Size(110, 18);
             this.label2.TabIndex = 141;
             this.label2.Text = "Ville souhaitée :";
             // 
             // nbPieces
             // 
-            this.nbPieces.Location = new System.Drawing.Point(288, 165);
+            this.nbPieces.Location = new System.Drawing.Point(384, 203);
+            this.nbPieces.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nbPieces.Name = "nbPieces";
-            this.nbPieces.Size = new System.Drawing.Size(66, 20);
+            this.nbPieces.Size = new System.Drawing.Size(88, 22);
             this.nbPieces.TabIndex = 157;
             // 
             // textBox_Prix
             // 
-            this.textBox_Prix.Location = new System.Drawing.Point(287, 222);
+            this.textBox_Prix.Location = new System.Drawing.Point(383, 273);
+            this.textBox_Prix.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_Prix.Name = "textBox_Prix";
-            this.textBox_Prix.Size = new System.Drawing.Size(195, 20);
+            this.textBox_Prix.Size = new System.Drawing.Size(259, 22);
             this.textBox_Prix.TabIndex = 159;
             // 
             // textBox_SurfaceHabitable
             // 
-            this.textBox_SurfaceHabitable.Location = new System.Drawing.Point(287, 103);
-            this.textBox_SurfaceHabitable.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_SurfaceHabitable.Location = new System.Drawing.Point(383, 127);
+            this.textBox_SurfaceHabitable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_SurfaceHabitable.Name = "textBox_SurfaceHabitable";
-            this.textBox_SurfaceHabitable.Size = new System.Drawing.Size(67, 20);
+            this.textBox_SurfaceHabitable.Size = new System.Drawing.Size(88, 22);
             this.textBox_SurfaceHabitable.TabIndex = 160;
             // 
             // textBox_SurfaceParcelle
             // 
-            this.textBox_SurfaceParcelle.Location = new System.Drawing.Point(287, 134);
-            this.textBox_SurfaceParcelle.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_SurfaceParcelle.Location = new System.Drawing.Point(383, 165);
+            this.textBox_SurfaceParcelle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_SurfaceParcelle.Name = "textBox_SurfaceParcelle";
-            this.textBox_SurfaceParcelle.Size = new System.Drawing.Size(67, 20);
+            this.textBox_SurfaceParcelle.Size = new System.Drawing.Size(88, 22);
             this.textBox_SurfaceParcelle.TabIndex = 161;
             // 
             // label5
@@ -99,9 +105,10 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Image = global::PTImmo_2018.Properties.Resources.icons8_to_do_48;
             this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label5.Location = new System.Drawing.Point(101, 24);
+            this.label5.Location = new System.Drawing.Point(135, 30);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(264, 53);
+            this.label5.Size = new System.Drawing.Size(352, 65);
             this.label5.TabIndex = 162;
             this.label5.Text = "         Nouveau souhait";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -111,9 +118,10 @@
             this.label19.AutoSize = true;
             this.label19.BackColor = System.Drawing.Color.Transparent;
             this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label19.Location = new System.Drawing.Point(130, 165);
+            this.label19.Location = new System.Drawing.Point(173, 203);
+            this.label19.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(101, 15);
+            this.label19.Size = new System.Drawing.Size(122, 18);
             this.label19.TabIndex = 165;
             this.label19.Text = "Nb Pieces (min) :";
             // 
@@ -122,9 +130,10 @@
             this.label18.AutoSize = true;
             this.label18.BackColor = System.Drawing.Color.Transparent;
             this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label18.Location = new System.Drawing.Point(130, 134);
+            this.label18.Location = new System.Drawing.Point(173, 165);
+            this.label18.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(134, 15);
+            this.label18.Size = new System.Drawing.Size(160, 18);
             this.label18.TabIndex = 164;
             this.label18.Text = "Surface parcelle (min) :";
             // 
@@ -133,9 +142,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(130, 104);
+            this.label1.Location = new System.Drawing.Point(173, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 15);
+            this.label1.Size = new System.Drawing.Size(167, 18);
             this.label1.TabIndex = 163;
             this.label1.Text = "Surface habitable (min) :";
             // 
@@ -144,9 +154,10 @@
             this.label23.AutoSize = true;
             this.label23.BackColor = System.Drawing.Color.Transparent;
             this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label23.Location = new System.Drawing.Point(130, 223);
+            this.label23.Location = new System.Drawing.Point(173, 274);
+            this.label23.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label23.Name = "label23";
-            this.label23.Size = new System.Drawing.Size(61, 15);
+            this.label23.Size = new System.Drawing.Size(73, 18);
             this.label23.TabIndex = 166;
             this.label23.Text = "Prix Max :";
             // 
@@ -154,33 +165,59 @@
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(133, 614);
+            this.button3.Location = new System.Drawing.Point(177, 756);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(159, 57);
+            this.button3.Size = new System.Drawing.Size(212, 70);
             this.button3.TabIndex = 224;
             this.button3.Text = "ANNULER";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Annuler_CreationSouhait);
             // 
             // button_Valider
             // 
             this.button_Valider.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_Valider.Image = global::PTImmo_2018.Properties.Resources.icons8_check_file_48;
-            this.button_Valider.Location = new System.Drawing.Point(451, 614);
+            this.button_Valider.Location = new System.Drawing.Point(601, 756);
+            this.button_Valider.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Valider.Name = "button_Valider";
-            this.button_Valider.Size = new System.Drawing.Size(159, 57);
+            this.button_Valider.Size = new System.Drawing.Size(212, 70);
             this.button_Valider.TabIndex = 223;
             this.button_Valider.Text = "VALIDER";
             this.button_Valider.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button_Valider.UseVisualStyleBackColor = true;
+            this.button_Valider.Click += new System.EventHandler(this.Validation_Creation_Souhait);
+            // 
+            // textBox_CP
+            // 
+            this.textBox_CP.Location = new System.Drawing.Point(383, 445);
+            this.textBox_CP.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_CP.Name = "textBox_CP";
+            this.textBox_CP.Size = new System.Drawing.Size(195, 22);
+            this.textBox_CP.TabIndex = 225;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(174, 449);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 18);
+            this.label3.TabIndex = 226;
+            this.label3.Text = "CP :";
             // 
             // FicheSouhaitCreation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PTImmo_2018.Properties.Resources.otherbackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(662, 726);
+            this.ClientSize = new System.Drawing.Size(883, 894);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_CP);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button_Valider);
             this.Controls.Add(this.label23);
@@ -195,6 +232,7 @@
             this.Controls.Add(this.textBox_Ville);
             this.Controls.Add(this.label2);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FicheSouhaitCreation";
             this.Text = "FicheSouhaitCreation";
             this.Load += new System.EventHandler(this.FicheSouhaitCreation_Load);
@@ -218,5 +256,7 @@
 		private System.Windows.Forms.Label label23;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Button button_Valider;
-	}
+        private System.Windows.Forms.TextBox textBox_CP;
+        private System.Windows.Forms.Label label3;
+    }
 }
--- PTImmo-2018/FicheSouhaitCreation.cs
+++ PTImmo-2018/FicheSouhaitCreation.cs
@@ -21,22 +21,28 @@ namespace PTImmo_2018
         private void Validation_Creation_Souhait(object sender, EventArgs e)
         {
 
-            string sql1 = "INSERT Into Souhait(NUM_ACHETEUR, SURFACE_HABITABLE_min, SURFACE_PARCELLE_min, NB_PIECES_min, Prix_max, STATUT, CODE_VILLE)";
-            string sql2 = "VALUES(" + Convert.ToInt32(textBox_SurfaceHabitable.Text) + "," + Convert.ToInt32(textBox_SurfaceParcelle.Text) + "," + Convert.ToInt32(nbPieces.Text) + ",'EN COURS', " + Convert.ToInt32(textBox_Ville.Text) + ");";
-
-            string sql = sql1 + sql2;
-
             string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-joyeux;Initial Catalog=IMMOBILLY_JACKYTEAM;Persist Security Info=True; Integrated Security=sspi;";
             OleDbConnection dbConnection = new OleDbConnection(ChaineBd);
             dbConnection.Open();
+
+            string sql1 = "INSERT Into Souhait(NUM_ACHETEUR, SURFACE_HABITABLE_min, SURFACE_PARCELLE_min, NB_PIECES_min, Prix_max, STATUT, CODE_VILLE)";
+            string sql2 = " VALUES('" + ApplicationState.id_acheteur + "', '" + textBox_SurfaceHabitable.Text + "','" + textBox_SurfaceParcelle.Text + "','" + nbPieces.Text + "','"+ textBox_Prix.Text + "','EN COURS', (SELECT v.code_ville from VILLE v where v.NOM_VILLE like  '" + textBox_Ville.Text + "' and v.CODE_POSTAL like '" + textBox_CP.Text + "') )";
+
+            string sql = sql1 + sql2;
+
+
             OleDbCommand cmd = new OleDbCommand(sql, dbConnection);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Saved");
+            visualiser_acheteur va = new visualiser_acheteur();
+            va.Show(this);
+            this.Hide();
+
 
         }
 
         private void Annuler_CreationSouhait(object sender, EventArgs e)
         {
-            visualiser_acheteur va = new visualiser_acheteur();
-            va.Show(this);
             this.Hide();
         }
 
@@ -52,5 +58,7 @@ namespace PTImmo_2018
             dbConnection.Open();
            
         }
+
+   
     }
 }
